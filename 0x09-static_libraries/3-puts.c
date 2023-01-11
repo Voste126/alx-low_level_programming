2469f05aaf227526bdb08c30c4a24a686873c8ce
voste126 <steveaustine126@gmail.com>
2023-01-11 06:34:48 -0800

    intial commit

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ *  _puts - writes a string to the standard output, followed by a newline
+ *  @str: pointer to a char string to write
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
